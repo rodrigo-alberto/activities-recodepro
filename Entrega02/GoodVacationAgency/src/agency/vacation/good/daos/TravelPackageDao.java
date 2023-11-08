2dfcedc41a7171ce5b24d1ccf61bc2039a3fdac5
Rodrigo Alberto <rodrigoalberto.m.sousa@gmail.com>
2023-11-07 21:11:24 -0300

    Update repository organization

--- Entrega02/GoodVacationAgency/src/agency/vacation/good/daos/TravelPackageDao.java
+++
@@ -1,5 +0,0 @@
-package agency.vacation.good.daos;
-
-public class TravelPackageDao {
-
-}
--- Entrega02/GoodVacationAgency/src/agency/vacation/good/main/Main.java
+++
@@ -1,9 +0,0 @@
-package agency.vacation.good.main;
-
-import agency.vacation.good.navigation.MainIO;
-
-public class Main {
-	public static void main(String[] args) {
-		MainIO.printMainMenu();
-	}
-}
